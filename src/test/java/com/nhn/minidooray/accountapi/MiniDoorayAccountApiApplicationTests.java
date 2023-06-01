5515b009b75be4840a4f696cd8d79de16b58fb7a
kimhyunjun <rlaguswns94@naver.com>
2023-06-01 20:15:21 +0900

    Initial commit

---
+++ src/test/java/com/nhn/minidooray/accountapi/MiniDoorayAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.minidooray.accountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDoorayAccountApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
