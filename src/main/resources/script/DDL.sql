244ca658b0817f08bc4c820badb7a4704fe3070f
kimhyunjun <rlaguswns94@naver.com>
2023-06-02 19:50:27 +0900

    DDL 파일 추가

---
+++ src/main/resources/script/DDL.sql
@@ -0,0 +1,53 @@
+USE nhn_academy_15;
+
+-- -----------------------------------------------------
+-- Table `nhn_academy_15`.`ACCOUNT`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `nhn_academy_15`.`ACCOUNT`
+(
+    `ID`            VARCHAR(40)  NOT NULL,
+    `PASSWORD`      VARCHAR(200) NOT NULL,
+    `SALT`          VARCHAR(45)  NOT NULL,
+    `NAME`          VARCHAR(20)  NOT NULL,
+    `EMAIL`         VARCHAR(100) NOT NULL,
+    `LAST_LOGIN_AT` DATETIME     NULL,
+    `CREATE_AT`     DATETIME     NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (`ID`)
+)
+    ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `nhn_academy_15`.`ACCOUNT_STATE`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `nhn_academy_15`.`ACCOUNT_STATE`
+(
+    `CODE`      CHAR(2)     NOT NULL,
+    `NAME`      VARCHAR(45) NOT NULL,
+    `CREATE_AT` DATETIME    NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (`CODE`)
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `nhn_academy_15`.`ACCOUNT_ACCOUNT_STATE`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `nhn_academy_15`.`ACCOUNT_ACCOUNT_STATE`
+(
+    `ACCOUNT_ID`         VARCHAR(40) NOT NULL,
+    `ACCOUNT_STATE_CODE` CHAR(2)     NOT NULL,
+    `CHANGE_AT`          DATETIME    NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (`ACCOUNT_ID`, `ACCOUNT_STATE_CODE`, `CHANGE_AT`),
+    INDEX `fk_ACCOUNT_ACCOUNT_STATE_ACCOUNT_STATE1_idx` (`ACCOUNT_STATE_CODE` ASC) VISIBLE,
+    CONSTRAINT `fk_ACCOUNT_ACCOUNT_STATE_ACCOUNT1`
+        FOREIGN KEY (`ACCOUNT_ID`)
+            REFERENCES `nhn_academy_15`.`ACCOUNT` (`ID`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `fk_ACCOUNT_ACCOUNT_STATE_ACCOUNT_STATE1`
+        FOREIGN KEY (`ACCOUNT_STATE_CODE`)
+            REFERENCES `nhn_academy_15`.`ACCOUNT_STATE` (`CODE`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB;
