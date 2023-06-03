1433316517b0938ca8c958992d711c439b7fb551
CHOIGANGHYUEN <goaqjrj13@gmail.com>
2023-06-03 16:39:21 +0900

    AccountAccountStateEntity 추가

---
+++ src/main/java/com/nhn/minidooray/accountapi/entity/AccountAccountStateEntity.java
@@ -0,0 +1,56 @@
+package com.nhn.minidooray.accountapi.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "ACCOUNT_ACCOUNT_STATE")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@EqualsAndHashCode
+@Generated
+public class AccountAccountStateEntity {
+@EmbeddedId
+  private AccountAccountStatePk accountAccountStatePk;
+@MapsId("accountId")
+@ManyToOne
+@JoinColumn(name = "ACCOUNT_ID",referencedColumnName = "ID",nullable = false)
+private AccountEntity account;
+@MapsId("accountStateCode")
+@ManyToOne
+@JoinColumn(name = "ACCOUNT_STATE_CODE",referencedColumnName = "CODE",nullable = false)
+private AccountStateEntity accountState;
+private LocalDateTime changeAt;
+@Embeddable
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Generated
+public static class AccountAccountStatePk implements Serializable {
+  private String accountId;
+  private String accountStateCode;
+  private LocalDateTime changeAt;
+}
+
+}
