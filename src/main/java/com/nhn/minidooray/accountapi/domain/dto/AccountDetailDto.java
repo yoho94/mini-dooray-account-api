205d84012a3057ba1c5bf83a065bada732ab1140
CHOIGANGHYUEN <goaqjrj13@gmail.com>
2023-06-03 17:14:05 +0900

    AccountDetailDto.java 추가

--- src/main/java/com/nhn/minidooray/accountapi/domain/dto/AccountDetailDto.java
+++ src/main/java/com/nhn/minidooray/accountapi/domain/dto/AccountDetailDto.java
@@ -1,5 +1,25 @@
 package com.nhn.minidooray.accountapi.domain.dto;
 
-public class AccountDetailDto {
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@EqualsAndHashCode
+@Generated
+public class AccountDetailDto {
+  private String id;
+  private String name;
+  private String email;
+  private LocalDateTime lastLoginAt;
+  private LocalDateTime createdAt;
 }
