a042e480c20437049923d861cb3c5ef5dbc248aa
CHOIGANGHYUEN <goaqjrj13@gmail.com>
2023-06-04 03:20:15 +0900

    뭐야 왜 커밋안됐어

--- src/main/java/com/nhn/minidooray/accountapi/domain/dto/AccountAccountStateDto.java
+++ src/main/java/com/nhn/minidooray/accountapi/domain/dto/AccountAccountStateDto.java
@@ -1,9 +1,30 @@
 package com.nhn.minidooray.accountapi.domain.dto;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@EqualsAndHashCode
+@Generated
 public class AccountAccountStateDto {
   public PkDto pkDto;
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  @Getter
+  @Setter
+  @EqualsAndHashCode
+  @Generated
   public static class PkDto {
     public String accountId;
     public String accountStateCode;
--- src/main/java/com/nhn/minidooray/accountapi/repository/AccountAccountStateRepository.java
+++ src/main/java/com/nhn/minidooray/accountapi/repository/AccountAccountStateRepository.java
@@ -2,8 +2,16 @@ package com.nhn.minidooray.accountapi.repository;
 
 import com.nhn.minidooray.accountapi.entity.AccountAccountStateEntity;
 import com.nhn.minidooray.accountapi.entity.AccountAccountStateEntity.Pk;
+import com.nhn.minidooray.accountapi.entity.AccountEntity;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountAccountStateRepository extends JpaRepository<AccountAccountStateEntity, Pk> {
+  public List<AccountAccountStateEntity> findAllByAccount_IdAndAccountState_Code(String accountId, String accountStateCode);
+ public List<AccountAccountStateEntity> findAllByAccountStateCode(String accountStateCode);
 
+  public List<AccountAccountStateEntity> findAllByAccount_Id(String accountId);
+
+  public List<AccountAccountStateEntity> findAllByAccount(List<AccountEntity> accountEntities);
 }
