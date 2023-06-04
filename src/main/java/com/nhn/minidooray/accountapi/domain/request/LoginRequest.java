e69e858b3dc24148d0112fe93ace7447c0a3911d
CHOIGANGHYUEN <goaqjrj13@gmail.com>
2023-06-04 21:04:25 +0900

    PasswordUtills.java

--- src/main/java/com/nhn/minidooray/accountapi/domain/request/LoginRequest.java
+++ src/main/java/com/nhn/minidooray/accountapi/domain/request/LoginRequest.java
@@ -2,11 +2,13 @@ package com.nhn.minidooray.accountapi.domain.request;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class LoginRequest {
   @NotEmpty
   @Size(min = 5, max = 40)
