786ff7cfec0fa4dd2a7107c4e27c6c4e22579164
CHOIGANGHYUEN <goaqjrj13@gmail.com>
2023-06-04 17:55:32 +0900

    PasswordUtills.java

--- src/main/java/com/nhn/minidooray/accountapi/service/impl/LoginService.java
+++ src/main/java/com/nhn/minidooray/accountapi/service/impl/LoginService.java
@@ -4,6 +4,7 @@ import com.nhn.minidooray.accountapi.domain.request.LoginRequest;
 import com.nhn.minidooray.accountapi.entity.AccountEntity;
 import com.nhn.minidooray.accountapi.repository.AccountRepository;
 import com.nhn.minidooray.accountapi.service.AccountService;
+import com.nhn.minidooray.accountapi.util.PasswordUtills;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,15 +18,10 @@ public class LoginService {
   public Optional<AccountEntity> loginValidate(LoginRequest loginRequest) {
     Optional<AccountEntity> account = accountRepository.findById(loginRequest.getId());
 
-    if (account.get().getPassword().equals(passwordEncoding(loginRequest.getPassword())))
+    if (account.get().getPassword().equals(PasswordUtills.encode(loginRequest.getPassword())))
       return Optional.of(account.get());
 
     return Optional.empty();
   }
-  private String passwordEncoding(String password) {
-    /**
-     * TODO 패스워드 암호화 규칙 적용
-     */
-    return password;
-  }
+
 }
