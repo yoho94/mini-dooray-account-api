7fb699eb4bde4ce3be70ac141df93f212b4ac958
kimhyunjun <rlaguswns94@naver.com>
2023-06-09 12:33:20 +0900

    account add unique

--- src/main/resources/script/DDL.sql
+++ src/main/resources/script/DDL.sql
@@ -6,12 +6,13 @@ USE nhn_academy_15;
 CREATE TABLE IF NOT EXISTS `nhn_academy_15`.`ACCOUNT`
 (
     `ID`            VARCHAR(40)  NOT NULL,
-    `PASSWORD`      VARCHAR(60) NOT NULL,
+    `PASSWORD`      VARCHAR(60)  NOT NULL,
     `NAME`          VARCHAR(20)  NOT NULL,
     `EMAIL`         VARCHAR(100) NOT NULL,
     `LAST_LOGIN_AT` DATETIME     NULL,
     `CREATE_AT`     DATETIME     NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (`ID`)
+    PRIMARY KEY (`ID`),
+    UNIQUE KEY `EMAIL` (`EMAIL`)
 )
     ENGINE = InnoDB;
 
