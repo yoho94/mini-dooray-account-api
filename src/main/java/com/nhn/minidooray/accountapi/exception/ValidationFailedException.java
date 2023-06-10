3e082d92da04b6f2ebd031e507808282dbd5f9e1
kimhyunjun <rlaguswns94@naver.com>
2023-06-10 16:29:38 +0900

    ValidationFailedException 수정

--- src/main/java/com/nhn/minidooray/accountapi/exception/ValidationFailedException.java
+++ src/main/java/com/nhn/minidooray/accountapi/exception/ValidationFailedException.java
@@ -3,9 +3,14 @@ package com.nhn.minidooray.accountapi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 
+import java.util.stream.Collectors;
+
 public class ValidationFailedException extends ApiException {
 
     public ValidationFailedException(BindingResult bindingResult) {
-        super(bindingResult.getFieldError().getDefaultMessage(), HttpStatus.UNPROCESSABLE_ENTITY.value());
+        super(bindingResult.getFieldErrors().stream()
+                        .map(f -> f.getField() + " : " + f.getDefaultMessage())
+                        .collect(Collectors.joining(",")),
+                HttpStatus.BAD_REQUEST.value());
     }
 }
