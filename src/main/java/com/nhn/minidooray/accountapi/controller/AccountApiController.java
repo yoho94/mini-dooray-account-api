4fabc911a19ac5001575ff499cfb797f5a829cdd
kimhyunjun <rlaguswns94@naver.com>
2023-06-11 23:09:26 +0900

    update 로직 삭제 된 것 다시 올림

--- src/main/java/com/nhn/minidooray/accountapi/controller/AccountApiController.java
+++ src/main/java/com/nhn/minidooray/accountapi/controller/AccountApiController.java
@@ -7,22 +7,16 @@ import com.nhn.minidooray.accountapi.domain.response.CommonResponse;
 import com.nhn.minidooray.accountapi.domain.response.ResultResponse;
 import com.nhn.minidooray.accountapi.exception.ValidationFailedException;
 import com.nhn.minidooray.accountapi.service.AccountService;
-import java.util.Collections;
-import java.util.List;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,70 +28,81 @@ public class AccountApiController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("${com.nhn.minidooray.accountapi.requestmapping.account.create-account}")
     public ResultResponse<Void> createAccount(@RequestBody @Valid AccountCreateRequest request,
-        BindingResult bindingResult) {
+                                              BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            throw new ValidationFailedException( bindingResult );
+            throw new ValidationFailedException(bindingResult);
         }
 
-        accountService.create( request );
-        return ResultResponse.created( null );
+        accountService.create(request);
+        return ResultResponse.created(null);
+
 
     }
 
+    @PostMapping("${com.nhn.minidooray.accountapi.requestmapping.account.update-account}")
+    public ResultResponse<Void> updateAccount(@PathVariable String id,
+                                              @RequestBody @Valid AccountUpdateRequest request,
+                                              BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationFailedException(bindingResult);
+        }
+
+        return accountService.update(id, request);
+    }
 
     @PutMapping("${com.nhn.minidooray.accountapi.requestmapping.account.update-account-name}")
     public ResultResponse<Void> updateAccountName(@PathVariable String id,
-        @RequestBody @Valid AccountUpdateRequest request, BindingResult bindingResult) {
+                                                  @RequestBody @Valid AccountUpdateRequest request, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            throw new ValidationFailedException( bindingResult );
+            throw new ValidationFailedException(bindingResult);
         }
 
-        accountService.updateName( id, request );
-        return ResultResponse.updated( null );
+        accountService.updateName(id, request);
+        return ResultResponse.updated(null);
 
     }
 
     @PutMapping("${com.nhn.minidooray.accountapi.requestmapping.account.update-account-password}")
     public ResultResponse<CommonResponse> updateAccountPasswordById(@PathVariable String id,
-        @RequestBody AccountUpdateRequest request, BindingResult bindingResult) {
+                                                                    @RequestBody AccountUpdateRequest request, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            throw new ValidationFailedException( bindingResult );
+            throw new ValidationFailedException(bindingResult);
         }
 
-        accountService.updatePassword( id, request );
-        return ResultResponse.updated( null );
+        accountService.updatePassword(id, request);
+        return ResultResponse.updated(null);
     }
 
     @GetMapping("${com.nhn.minidooray.accountapi.requestmapping.account.update-last-login-at}")
     public ResultResponse<Void> updateLastLoginAtById(@PathVariable String id) {
-        accountService.updateByLastLoginAt( id );
-        return ResultResponse.updated( null );
+        accountService.updateByLastLoginAt(id);
+        return ResultResponse.updated(null);
 
     }
 
     @GetMapping("${com.nhn.minidooray.accountapi.requestmapping.account.read-account-list}")
     public ResultResponse<List<AccountResponse>> getAll(Pageable pageable) {
-        Page<AccountResponse> result = accountService.getAll( pageable );
-        return ResultResponse.fetched( Collections.singletonList( result.getContent() ) );
+        Page<AccountResponse> result = accountService.getAll(pageable);
+        return ResultResponse.fetched(Collections.singletonList(result.getContent()));
     }
 
 
     @GetMapping("${com.nhn.minidooray.accountapi.requestmapping.account.read-account-by-id}")
     public ResultResponse<AccountResponse> getAccount(@PathVariable("id") String id) {
 
-        AccountResponse account = accountService.get( id );
+        AccountResponse account = accountService.get(id);
 
-        return ResultResponse.fetched( Collections.singletonList( account ) );
+        return ResultResponse.fetched(Collections.singletonList(account));
     }
 
     @GetMapping("${com.nhn.minidooray.accountapi.requestmapping.account.read-account-by-email}")
     public ResultResponse<AccountResponse> getAccountByEmail(@PathVariable("email") String email) {
 
-        AccountResponse account = accountService.findByEmail( email );
+        AccountResponse account = accountService.findByEmail(email);
 
-        return ResultResponse.fetched( Collections.singletonList( account ) );
+        return ResultResponse.fetched(Collections.singletonList(account));
 
     }
 
